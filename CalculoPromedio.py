e41fa08fd95a13a1b248fd444ef5107175f7ff85
bryanmino <66659974+bryanmino@users.noreply.github.com>
2020-08-04 23:45:12 -0500

    Evaluar si el promedio es mayor que 7

--- CalculoPromedio.py
+++ CalculoPromedio.py
@@ -2,3 +2,7 @@ nota1 = int(input("Ingrese la Nota 1 "))
 nota2 = int(input("Ingrese la Nota 2 "))
 nota3 = int(input("Ingrese la Nota 3 "))
 promedio = (nota1 + nota2 + nota3) / 3
+if promedio >= 7:
+    print("Aprobado")
+else:
+    print("Reprobado")
